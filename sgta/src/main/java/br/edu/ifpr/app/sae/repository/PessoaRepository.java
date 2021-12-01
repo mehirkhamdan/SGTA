f5d66de40dbd4728877cd873398f46eb0835614f
mehir <mehir.hamdan@gmail.com>
2021-11-30 21:32:32 -0300

    create branch maste

---
+++ sgta/src/main/java/br/edu/ifpr/app/sae/repository/PessoaRepository.java
@@ -0,0 +1,10 @@
+package br.edu.ifpr.app.sae.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.edu.ifpr.app.sae.model.Pessoa;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+	
+	
+}
